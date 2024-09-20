829a3f62997bb319906800f60895407462f0735e
Reginaldo Oliveira <123994970+rjmodev@users.noreply.github.com>
2024-09-20 09:56:41 -0300

    Delete DIO/trilha_java_basico/LearningSintaxJava/AnatomyOfClass directory
    fixed file course java

--- DIO/trilha_java_basico/LearningSintaxJava/AnatomyOfClass/MyClass.java
+++
@@ -1,8 +0,0 @@
-//Sempre que criar a classe, deve ser com inicial UPPER e com o mesmo nome do arquivo.
-public class MyClass {
-    //Deve-se criar métodos  para estruturar a classe no arquivo e realizar as funçoes especificas
-    public static void main(String [] args) {
-        //Um exemplo de impressão de uma expressão em tela
-        System.out.println("Hello World! Nice to meet you!");
-    }
-}
