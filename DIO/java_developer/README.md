33bc4b71c3a2a979da14620782bd0bff1b706f31
Reginaldo Oliveira <123994970+rjmodev@users.noreply.github.com>
2024-09-20 11:09:21 -0300

    course Dio Java Developer
    Include Course Java  Developer

---
+++ DIO/java_developer/README.md
@@ -0,0 +1,2 @@
+# Dio Java Developer
+Repositório para armazenar todo o código do curso de Java Developer
---
+++ DIO/java_developer/src/java_fundamentals/learnig_sintax/anatomy_of_class/HelloWorld.java
@@ -0,0 +1,10 @@
+package java_fundamentals.learnig_sintax.anatomy_of_class;
+
+//Sempre que criar a classe, deve ser com inicial UPPER e com o mesmo nome do arquivo.
+public class HelloWorld {
+    //Deve-se criar métodos  para estruturar a classe no arquivo e realizar as funçoes especificas
+    public static void main(String[] args) {
+        //Um exemplo de impressão de uma expressão em tela
+        System.out.println("Hello World! Nice to meet you!");
+    }
+}
