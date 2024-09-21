556aa8e4e60a06420a7c7db0aaba156ef6a4386e
rjmodev <psreginaldo@hotmail.com>
2024-09-21 19:27:13 -0300

    update and add archives

---
+++ DIO/java_developer/src/java_fundamentals/learnig_sintax/anatomy_of_class/NomeVariavel.java
@@ -0,0 +1,29 @@
+package java_fundamentals.learnig_sintax.anatomy_of_class;
+
+public class NomeVariavel {
+    public static void main(String[] args) {
+        //O uso de letras maiusculas em variaveis é uma convenção para o tipo de váriavel constante
+        String BR = "Brasil";
+        //Para garantir uma váriavel constante é utilizado uma palavra reservada: final
+        final String CH = "China";
+        int ano = 2024;
+        //Outros Exemplos:
+        int TOTAL_ESTADOS_BRASILEIROS = 27;
+        double PI = 3.14;
+        String nomeAluno = "Roberto";
+
+        //Declaração válida de variáveis
+        int numero$um = 1;
+        int numero1 = 1;
+        int numeroum = 1;
+        int longo = 1;
+    }
+}
+
+
+
+
+
+
+
+
