f5c71bb121fb2604544d8117df20a34a6cf0d3e4
rjmodev <psreginaldo@hotmail.com>
2024-10-03 23:56:59 -0300

    update course java_developer/add operator

--- DIO/java_developer/src/edu/java_fundamentals/learnig_sintax/operator/TypesOperator.java
+++ DIO/java_developer/src/edu/java_fundamentals/learnig_sintax/operator/TypesOperator.java
@@ -1,4 +1,15 @@
 package edu.java_fundamentals.learnig_sintax.operator;
 
+import java.util.Date;
+
 public class TypesOperator {
+    public static void main(String[] args) {
+        //classe TypesOperator.java
+        String nome = "Diogo";
+        int idade = 32;
+        double peso = 73.4;
+        char sexo = 'F';
+        boolean doadorPecas = false;
+        Date dataNascimento = newDate();
+    }
 }
